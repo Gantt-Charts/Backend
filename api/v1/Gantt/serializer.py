5ace2ce14eb48afc1dd190a5329a8090c29034c8
Aleksey/Bishopik <deadkniga@gmail.com>
2024-03-04 06:34:31 +0300

    replic

--- api/v1/Gantt/serializer.py
+++ api/v1/Gantt/serializer.py
@@ -24,7 +24,12 @@ class ProjectTaskSerializer(serializers.ModelSerializer):
     def update(self, instance, validated_data):
         dependencies = validated_data.pop('dependencies')
         instance.name = validated_data.get('name', instance.name)
-        instance.description = validated_data.get('description', instance.description)
-        instance.save()
+        instance.type = validated_data.get('type', instance.type)
+        instance.start = validated_data.get('start', instance.start)
+        instance.end = validated_data.get('end', instance.end)
+        instance.progress = validated_data.get('progress', instance.progress)
+        instance.isDisabled = validated_data.get('isDisabled', instance.isDisabled)
+        instance.project = validated_data.get('project', instance.project)
         instance.dependencies.set(dependencies)
+        instance.save()
         return instance
